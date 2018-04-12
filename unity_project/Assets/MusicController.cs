c91291ce27cfa281a851e746a07714ab91d7b3cb
Adrian Riedel <adrian.riedel@outlook.com>
2018-04-13 00:33:54 +0200

    added MusicController for first planet

---
+++ unity_project/Assets/MusicController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEditor;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+    GameObject PlayerToFollow;
+    GameObject MusicWall1;
+    GameObject BackgroundMusic;
+    new AudioSource audio;
+
+    int part = 0;
+
+	// Use this for initialization
+	void Start () {
+        PlayerToFollow = GameObject.FindGameObjectWithTag("Player");
+        MusicWall1 = GameObject.Find("MusicWall1");
+        BackgroundMusic = GameObject.Find("BackgroundMusic");
+        audio = BackgroundMusic.GetComponent<AudioSource>();
+        PlayAudio();
+	}
+
+    private void PlayAudio()
+    {
+        string path = string.Format("Assets/_music/first_planet/{0}.ogg", part);
+        var clip = AssetDatabase.LoadAssetAtPath<AudioClip>(path);
+        audio.PlayOneShot(clip);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (audio.isPlaying)
+            return;
+
+        bool loop1 = part == 1 || part == 2;
+        bool part2 = PlayerToFollow.transform.position.x > MusicWall1.transform.position.x;
+
+        if (part2 && loop1)
+        {
+            part = 3;
+            PlayAudio();
+            return;
+        }
+
+        if (part == 0 || part == 3)
+            part++;
+
+        if (loop1)
+        {
+            if (part == 1)
+                part++;
+            else
+                part--;           
+        }
+        PlayAudio();
+    }
+}
