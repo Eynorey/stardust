11c7805bd925a68bb9ae8a16b3f93db0fe1675a7
Adrian Riedel <adrian.riedel@outlook.com>
2018-04-26 16:25:39 +0200

    adjusted script to accommodate for other scenes where video should not be played back

--- unity_project/Assets/Scripts/LoadSceneOnClick.cs
+++ unity_project/Assets/Scripts/LoadSceneOnClick.cs
@@ -10,29 +10,39 @@ public class LoadSceneOnClick : MonoBehaviour {
     public GameObject loadingBar;
     public Slider slider;
     AsyncOperation sceneLoading;
+    bool playIntro;
+    VideoPlayer video;
 
     public void LoadLevel(int sceneIndex)
     {
+        if (sceneIndex == 1)
+        {
+            playIntro = true;
+            video = loadingBar.GetComponent<VideoPlayer>();
+        }
+
         sceneLoading = SceneManager.LoadSceneAsync(sceneIndex);
         sceneLoading.allowSceneActivation = false;
-        StartCoroutine(LoadAsynchronously(sceneIndex));
+        StartCoroutine(LoadAsynchronously(sceneIndex));    
     }
 
     IEnumerator LoadAsynchronously(int sceneIndex)
     {
-
-        GameObject.Find("MainMenuePanel").SetActive(false);
         loadingBar.SetActive(true);
-        VideoPlayer video = loadingBar.GetComponent<VideoPlayer>();
-        video.Prepare();
-        while (!video.isPrepared)
-        {
-            yield return null;
-        }
-        GameObject.Find("Music").GetComponent<AudioSource>().Stop();
-        video.Play();
 
-        while (sceneLoading.progress < 0.9f || video.isPlaying)
+        if (playIntro)
+        {
+            GameObject.Find("MainMenuePanel").SetActive(false);
+            video.Prepare();
+            while (!video.isPrepared)
+            {
+                yield return null;
+            }
+            GameObject.Find("Music").GetComponent<AudioSource>().Stop();
+            video.Play();
+        }
+
+        while (sceneLoading.progress < 0.9f || (playIntro && video.isPlaying))
         {
             slider.value = sceneLoading.progress / .9f;
             yield return null;
